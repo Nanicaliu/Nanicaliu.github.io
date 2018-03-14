8c118194391a16c6ef17521a56f927ee05a8bfaa
Nanica <liulanm518@outlook.com>
2018-03-14 10:43:25 -0700

    Fill the content in the main page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(window).load(function(){
 	$('#preloader').fadeOut('slow',function(){$(this).remove();});
 });
 
+/******************************************************************************************************************************
+Elevator Float
+*******************************************************************************************************************************/
 
 $(window).scroll(function(){
  if($(window).scrollTop() >= 895){
@@ -10,6 +13,9 @@ $(window).scroll(function(){
   $("#project-elevator-container2").hide(); 
  }
 });
+
+
+
 
 /******************************************************************************************************************************
 Learn More Page Scroll
@@ -68,5 +74,59 @@ Menu
 	init();
 
 })();
+
+
+/******************************************************************************************************************************
+Slides Show
+*******************************************************************************************************************************/
+
+
+// Automatic Slide Show
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none"; 
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1} 
+//     slides[slideIndex-1].style.display = "block"; 
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+
+// Slide show
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
 
 
